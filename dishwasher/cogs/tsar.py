8946cd80312f50af11ad9bfe7235e4f4c6e7c178
vrnavi <lapis@starrypoint.me>
2023-07-15 23:22:44 -0400

    misery

--- dishwasher/cogs/tsar.py
+++ dishwasher/cogs/tsar.py
@@ -1,6 +1,7 @@
 import config
 import datetime
 import discord
+import json
 from discord.ext import commands, tasks
 from discord.ext.commands import Cog
 from helpers.checks import check_if_staff, check_if_bot_manager
@@ -50,10 +51,10 @@ class TSAR(Cog):
             for name, tsar in list(configs["tsar"]["roles"].items()):
                 embed.add_field(
                     name=name,
-                    value=f"**Role:** {ctx.guild.get_role(int(tsar['roleid'])).mention}\n"
+                    value=f"**Role:** {ctx.guild.get_role(tsar['roleid']).mention}\n"
                     + f"**Minimum Days:** `{tsar['mindays']}`\n"
-                    + f"**Forbidden Roles:** {' '.join(ctx.guild.get_role(s).mention for s in tsar['blacklisted']) if tsar['blacklisted'] else 'None'}\n"
-                    + f"**Required Roles:** {' '.join(ctx.guild.get_role(s).mention for s in tsar['required']) if tsar['required'] else 'None'}",
+                    + f"**Forbidden Roles:** {', '.join([ctx.guild.get_role(s).mention for s in tsar['blacklisted']]) if tsar['blacklisted'] else 'None'}\n"
+                    + f"**Required Roles:** {', '.join([ctx.guild.get_role(s).mention for s in tsar['required']]) if tsar['required'] else 'None'}",
                     inline=False,
                 )
         configmsg = await ctx.reply(embed=embed, mention_author=False)
