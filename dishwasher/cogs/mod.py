2ce417e8ad32ff6a406332b76b0770c94f407480
vrnavi <lapis@starrypoint.me>
2023-07-29 19:41:50 -0400

    restrict arbitrary command usage

--- dishwasher/cogs/mod.py
+++ dishwasher/cogs/mod.py
@@ -738,10 +738,11 @@ class Mod(Cog):
     ):
         """[S] Posts a given text in a given channel."""
         output = await channel.send(the_text)
-        output.author = ctx.author
-        newctx = await self.bot.get_context(output)
-        newctx.message.author = ctx.guild.me
-        await self.bot.invoke(newctx)
+        if ctx.author.id in config.bot_managers:
+            output.author = ctx.author
+            newctx = await self.bot.get_context(output)
+            newctx.message.author = ctx.guild.me
+            await self.bot.invoke(newctx)
         await ctx.message.reply("👍", mention_author=False)
 
     @commands.guild_only()
@@ -758,10 +759,11 @@ class Mod(Cog):
         """[S] Replies to a message with a given text in a given channel."""
         msg = await channel.fetch_message(message)
         output = await msg.reply(content=f"{the_text}", mention_author=False)
-        output.author = ctx.author
-        newctx = await self.bot.get_context(output)
-        newctx.message.author = ctx.guild.me
-        await self.bot.invoke(newctx)
+        if ctx.author.id in config.bot_managers:
+            output.author = ctx.author
+            newctx = await self.bot.get_context(output)
+            newctx.message.author = ctx.guild.me
+            await self.bot.invoke(newctx)
         await ctx.message.reply("👍", mention_author=False)
 
     @commands.guild_only()
