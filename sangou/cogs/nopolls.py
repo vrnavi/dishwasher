915e7082eb3a26a97ad150fc01cc1127ce24473b
renavi <ren@0ccu.lt>
2024-03-22 00:17:31 -0400

    hefty cleanup of nopolls

--- sangou/cogs/nopolls.py
+++ sangou/cogs/nopolls.py
@@ -11,41 +11,44 @@ class Nopolls(Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    async def poll_check(self, payload):
-        channel_id = payload["channel_id"]
-        user_id = payload["author"]["id"]
-        message_id = payload["id"]
-        guild_id = payload.get("guild_id", None)
-        if "poll" not in payload:
+    @Cog.listener()
+    async def on_socket_raw_receive(self, msg):
+        msg_json = json.loads(msg)
+        opcode = msg_json["op"]
+        event = msg_json["t"]
+        payload = msg_json["d"]
+
+        if (
+            opcode != 0
+            or event != "MESSAGE_CREATE"
+            or "poll" not in payload
+            or "guild_id" not in payload
+            or not payload["poll"]
+            or not payload["guild_id"]
+        ):
             return
-        poll = payload["poll"]
 
-        # Ignore not super reactions or DM reaction add events
-        if not poll or not guild_id:
+        guild = self.bot.get_guild(int(payload["guild_id"]))
+        channel = guild.get_channel_or_thread(int(payload["channel_id"]))
+        author = guild.get_member(int(payload["author"]["id"]))
+        message = channel.get_partial_message(int(payload["id"]))
+
+        # Ignore unconfigured guilds is leeching off of burstreacts for now
+        if not get_config(guild.id, "reaction", "burstreactsenable"):
             return
 
-        # Ignore not configured guilds
-        # leeching off of burstreacts for now
-        if not get_config(guild_id, "reaction", "burstreactsenable"):
-            return
-
-        guild = self.bot.get_guild(int(guild_id))
-        channel = guild.get_channel_or_thread(int(channel_id))
-        author = guild.get_member(int(user_id))
-        message = channel.get_partial_message(int(message_id))
-
-        # Remove message
         await message.delete()
 
         mlog = self.bot.pull_channel(guild, get_config(guild.id, "logging", "modlog"))
-
         if not mlog:
             return
 
         # Send information to log channel
         embed = stock_embed(self.bot)
         embed.title = "🗑️ Autoremoved a Poll"
-        embed.description = f"`{author}`'s poll was removed. [{message.channel.mention}]"
+        embed.description = (
+            f"`{author}`'s poll was removed. [{message.channel.mention}]"
+        )
         embed.color = 0xEA50BA
         embed.set_author(
             name=self.bot.escape_message(author),
@@ -53,16 +56,6 @@ class Nopolls(Cog):
         )
 
         await mlog.send(embed=embed)
-
-    @Cog.listener()
-    async def on_socket_raw_receive(self, msg: str):
-        msg_json = json.loads(msg)
-        opcode = msg_json["op"]
-        event = msg_json["t"]
-        data = msg_json["d"]
-
-        if opcode == 0 and event == "MESSAGE_CREATE":
-            await self.poll_check(data)
 
 
 async def setup(bot):
